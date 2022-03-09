3d7a85e142fb55773a71e9d8597c9c37aa568072
Ionut Muthi <ionut.muthi@analog.com>
2022-03-09 13:50:52 +0200

    added data logger functionality
    Signed-off-by: Ionut Muthi <ionut.muthi@analog.com>

---
+++ src/gui/dataloggerui.cpp
@@ -0,0 +1,215 @@
+#include "dataloggerui.hpp"
+
+using namespace adiscope;
+
+DataLoggerUI::DataLoggerUI(bool lastValue, bool average, bool all, QWidget *parent) :
+	QWidget(parent)
+{
+
+	dataLoggingWidget = new QWidget(this);
+	dataLoggingLayout = new QVBoxLayout(dataLoggingWidget);
+
+	dataLoggerFilter = new QComboBox(dataLoggingWidget);
+	if(lastValue) {	dataLoggerFilter->addItem(QString("Last Value")); }
+	if(average) {	dataLoggerFilter->addItem(QString("Average")); }
+	if(all) {	dataLoggerFilter->addItem(QString("All")); }
+	dataLoggerFilter->setCurrentIndex(0);
+	dataLoggerFilter->setDisabled(true);
+
+	init();
+}
+
+
+void DataLoggerUI::init()
+{
+	dataLoggingSwitch = new CustomSwitch(dataLoggingWidget);
+	dataLoggingLayout->addWidget(dataLoggingSwitch);
+	dataLoggingLayout->setAlignment(dataLoggingSwitch,Qt::AlignRight);
+
+	dataLoggingLayout->addWidget(new QLabel("Choose file"));
+	dataLoggingFilePath = new QLineEdit(dataLoggingWidget);
+	dataLoggingFilePath->setReadOnly(true);
+	dataLoggingFilePath->setDisabled(true);
+
+	connect(dataLoggingFilePath, &QLineEdit::textChanged, this, [=](QString path){
+		if(filename.isEmpty() && dataLoggingFilePath->isEnabled()) {
+			dataLoggingFilePath->setText(tr("No file selected"));
+			dataLoggingFilePath->setStyleSheet("color:red");
+		}else{
+			dataLoggingFilePath->setStyleSheet("color:white");
+			Q_EMIT pathChanged(path);
+		}
+	});
+
+	dataLoggingLayout->addWidget(dataLoggingFilePath);
+	dataLogginBrowseBtn = new QPushButton("Browse");
+	dataLogginBrowseBtn->setProperty("blue_button",true);
+	dataLogginBrowseBtn->setMinimumHeight(30);
+	dataLogginBrowseBtn->setDisabled(true);
+	dataLoggingLayout->addWidget(dataLogginBrowseBtn);
+
+	connect(dataLogginBrowseBtn, &QPushButton::clicked,
+			this, [=](){
+		chooseFile();
+	});
+
+	overwriteRadio = new QRadioButton("Overwrite");
+	overwriteRadio->setChecked(true);
+	overwriteRadio->setDisabled(true);
+	dataLoggingLayout->addWidget(overwriteRadio);
+	appendRadio = new QRadioButton("Append");
+	dataLoggingLayout->addWidget(appendRadio);
+	appendRadio->setDisabled(true);
+
+	connect(overwriteRadio, &QRadioButton::toggled, [=](bool en) {
+		appendRadio->setChecked(!en);
+	});
+
+	connect(appendRadio, &QRadioButton::toggled, [=](bool en) {
+		overwriteRadio->setChecked(!en);
+	});
+
+	data_logging_timer = new PositionSpinButton({
+																		{"s", 1},
+																		{"min", 60},
+																		{"h", 3600}
+																	}, tr("Timer"), 0, 3600,
+																	true, false, this);
+
+	data_logging_timer->setValue(5);
+	data_logging_timer->setDisabled(true);
+
+	connect(data_logging_timer, &PositionSpinButton::valueChanged, this, [=](){
+		Q_EMIT timeIntervalChanged(data_logging_timer->value() * 1000); //converts to seconds before emiting value
+	});
+
+	dataLoggingLayout->addWidget(data_logging_timer);
+	dataLoggingLayout->addWidget(dataLoggerFilter);
+
+
+	warningMessage = new QLabel("");
+	warningMessage->setStyleSheet("color:white;");
+	warningMessage->setWordWrap(true);
+	warningMessage->setVisible(false);
+
+	dataLoggingLayout->addWidget(warningMessage);
+
+	//on data logging switch pressed enable/disable data logging section and emit data loggin toggled
+	connect(dataLoggingSwitch,  &CustomSwitch::toggled, this, [=](bool toggled){
+		Q_EMIT toggleDataLogger(toggled);
+		enableDataLoggerFields(toggled);
+	});
+
+}
+
+void DataLoggerUI::chooseFile()
+{
+	QString selectedFilter;
+
+	filename = QFileDialog::getSaveFileName(this,
+											tr("Export"), "", tr("Comma-separated values files (*.csv);;All Files(*)"),
+											&selectedFilter, QFileDialog::Options());
+	dataLoggingFilePath->setText(filename);
+}
+
+
+QWidget* DataLoggerUI::getDataLoggerUIWidget()
+{
+	return dataLoggingWidget;
+}
+
+
+bool DataLoggerUI::isDataLoggingOn()
+{
+	return dataLoggingSwitch->isChecked();
+}
+
+bool DataLoggerUI::isOverwrite()
+{
+	return overwriteRadio->isChecked();
+}
+
+QString DataLoggerUI::getFilter()
+{
+	return dataLoggerFilter->currentText();
+}
+
+void DataLoggerUI::setWarningMessage(QString message)
+{
+	warningMessage->setText(message);
+}
+
+void DataLoggerUI::enableDataLoggerFields(bool en)
+{
+	dataLoggingFilePath->setDisabled(!en);
+	dataLogginBrowseBtn->setDisabled(!en);
+	overwriteRadio->setDisabled(!en);
+	appendRadio->setDisabled(!en);
+	data_logging_timer->setDisabled(!en);
+	dataLoggerFilter->setDisabled(!en);
+	warningMessage->setVisible(en);
+
+	if(filename.isEmpty() && dataLoggingFilePath->isEnabled()) {
+		dataLoggingFilePath->setText(tr("No file selected"));
+		dataLoggingFilePath->setStyleSheet("color:red");
+	}
+}
+
+void DataLoggerUI::disableDataLogging(bool en)
+{
+	dataLoggingSwitch->setEnabled(!en);
+}
+
+void DataLoggerUI::toggleDataLoggerSwitch(bool toggle)
+{
+	dataLoggingSwitch->setChecked(toggle);
+}
+
+void DataLoggerUI::setDataLoggerPath(QString path)
+{
+	filename = path;
+	dataLoggingFilePath->setText(path);
+}
+
+QString DataLoggerUI::getDataLoggerPath()
+{
+	return filename;
+}
+
+void DataLoggerUI::setOverwrite(bool en)
+{
+	if(en){
+		overwriteRadio->setChecked(true);
+	}else{
+		appendRadio->setChecked(true);
+	}
+}
+
+int DataLoggerUI::getTimerInterval()
+{
+	return data_logging_timer->value();
+}
+
+void DataLoggerUI::setTimerInterval(int interval)
+{
+	data_logging_timer->setValue(interval);
+}
+
+void DataLoggerUI::isDataLoggerRunning(bool en)
+{
+	overwriteRadio->setDisabled(en);
+	appendRadio->setDisabled(en);
+}
+
+DataLoggerUI::~DataLoggerUI()
+{
+	delete dataLoggingWidget;
+	delete dataLoggingLayout;
+	delete dataLoggingFilePath;
+	delete overwriteRadio;
+	delete appendRadio;
+	delete dataLoggingSwitch;
+	delete dataLoggerFilter;
+	delete dataLogginBrowseBtn;
+}
+
---
+++ src/gui/dataloggerui.hpp
@@ -0,0 +1,62 @@
+#ifndef DATALOGGERUI_HPP
+#define DATALOGGERUI_HPP
+
+#include <QWidget>
+#include <QLineEdit>
+#include <QRadioButton>
+#include "customSwitch.hpp"
+#include <QVBoxLayout>
+#include "spinbox_a.hpp"
+#include <QFileDialog>
+#include <qcombobox.h>
+#include <QLabel>
+
+namespace adiscope {
+class DataLoggerUI : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit DataLoggerUI(bool lastValue, bool average, bool all, QWidget *parent = nullptr);
+	~DataLoggerUI();
+
+	//functions
+	void init();
+	void chooseFile();
+	QWidget* getDataLoggerUIWidget();
+	bool isDataLoggingOn();
+	bool isOverwrite();
+	QString getFilter();
+	void toggleDataLoggerSwitch(bool toggle);
+	void setDataLoggerPath(QString path);
+	QString getDataLoggerPath();
+	void setOverwrite(bool en);
+	int getTimerInterval();
+	void setTimerInterval(int interval);
+	void isDataLoggerRunning(bool en);
+
+	void setWarningMessage(QString message);
+	void enableDataLoggerFields(bool en);
+	void disableDataLogging(bool en);
+
+private:
+	PositionSpinButton *data_logging_timer;
+	QWidget *dataLoggingWidget;
+	QVBoxLayout *dataLoggingLayout;
+	QLineEdit *dataLoggingFilePath;
+	QRadioButton *overwriteRadio;
+	QRadioButton *appendRadio;
+	CustomSwitch *dataLoggingSwitch;
+	QComboBox *dataLoggerFilter;
+	QString filename;
+	QLabel *warningMessage;
+	QPushButton *dataLogginBrowseBtn;
+
+Q_SIGNALS:
+	void toggleDataLogger(bool toggled);
+	void pathChanged(QString path);
+	void timeIntervalChanged(double inetrval);
+
+};
+}
+#endif // DATALOGGERUI_HPP
